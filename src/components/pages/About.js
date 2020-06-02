e6393eda9c257645e7a2ce72da3cea3b521c3e1f
Kartik Kher <kartikkher777@gmail.com>
2020-06-02 19:02:05 +0100

    WIP: About page, router, links

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>App to search GitHub users</p>
+      <p>Version: 2.0.0</p>
+      <p>Author: Kartik Kher</p>
+      <p>
+        Inspired and Developed using <strong>Brad Traversy</strong> Udemy Course
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
